1ab08ebc7b4916434dc9c9eb08e039ffc0c5b8d0
Stevien04 <Stemarcaa@upt.pe>
2025-10-30 14:39:25 -0500

    sma

--- integraupt-backend/src/main/java/com/integraupt/controlador/clsControladorEspacio.java
+++ integraupt-backend/src/main/java/com/integraupt/controlador/clsControladorEspacio.java
@@ -7,16 +7,27 @@ import com.integraupt.servicio.clsServicioEspacio;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
+
 
 /**
  * Controlador REST para la gestión de espacios (aulas y laboratorios)
  */
 @RestController
 @RequestMapping("/api/espacios")
+@CrossOrigin(origins = "*")
 public class clsControladorEspacio {
 
     private final clsServicioEspacio servicioEspacio;
